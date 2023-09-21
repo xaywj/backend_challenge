637e7a7523b5321756628b0d79b0ecc7368de561
xa <87640952+xaywj@users.noreply.github.com>
2023-09-21 23:00:17 +0700

    add validate create user

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,21 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  phone: string;
+
+  @IsNotEmpty()
+  @Length(2, 20)
+  username: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+
+  @IsNotEmpty()
+  role: string;
+}
