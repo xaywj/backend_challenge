6d17bb2655f1707e869fdf3f147628ef51ec9f0e
xa <87640952+xaywj@users.noreply.github.com>
2023-09-22 00:46:06 +0700

    create format to respone when success

---
+++ src/handle/handle.respone.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class ResponeInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(map(data => ({
+        success:true,
+        data:data,
+        message:"success",
+        path:context.switchToHttp().getRequest().url
+    })));
+  }
+}
