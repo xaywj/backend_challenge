1a7c0eb64dfa5e1202db8e47cc4eed6c65948403
xa <87640952+xaywj@users.noreply.github.com>
2023-09-21 22:52:12 +0700

    add function to create table(user-rate-product)

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 12 })
+  phone: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 20 })
+  role: string;
+}
